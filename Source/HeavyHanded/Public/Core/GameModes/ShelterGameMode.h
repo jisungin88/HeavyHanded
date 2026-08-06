9262984fad80b3617ed93176569114941d194723
LEEEJJJJ <26247306+LJEeee@users.noreply.github.com>
2026-08-06 13:39:48 +0900

    feat : 네트워크(타이틀) 추가 중

---
+++ Source/HeavyHanded/Public/Core/GameModes/ShelterGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ShelterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEAVYHANDED_API AShelterGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
