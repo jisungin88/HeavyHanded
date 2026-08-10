2277849357da8c89283fa7eeed88c4ca6e47609e
LEEEJJJJ <26247306+LJEeee@users.noreply.github.com>
2026-08-10 17:30:23 +0900

    feat : 임시 로비 채팅 시스템 추가 , ui 임시 가이드 배치 (타이틀, 로비만)

--- Source/HeavyHanded/Private/Core/PlayerControllers/ShelterPlayerController.cpp
+++ Source/HeavyHanded/Private/Core/PlayerControllers/ShelterPlayerController.cpp
@@ -3,66 +3,65 @@
 
 #include "Core/PlayerControllers/ShelterPlayerController.h"
 #include "Core/GameStates/ShelterGameState.h"
-#include "GameFramework/PlayerState.h"
+#include "Core/PlayerStates/ShelterPlayerState.h"
 
 void AShelterPlayerController::BeginPlay()
 {
     Super::BeginPlay();
 
-
-    //if (IOnlineSubsystem* OSS = IOnlineSubsystem::Get())
-    //{
-    //    SessionInterface = OSS->GetSessionInterface();
-    //}
-    //
-    //if (SessionInterface.IsValid())
-    //{
-    //    FNamedOnlineSession* Session =
-    //        SessionInterface->GetNamedSession(NAME_GameSession);
-    //
-    //    if (Session)
-    //    {
-    //        UE_LOG(LogTemp, Warning,
-    //            TEXT("After Travel Session Exists"));
-    //
-    //        UE_LOG(LogTemp, Warning,
-    //            TEXT("State = %d"),
-    //            (int32)Session->SessionState);
-    //
-    //        UE_LOG(LogTemp, Warning,
-    //            TEXT("Advertise = %d"),
-    //            Session->SessionSettings.bShouldAdvertise);
-    //
-    //        UE_LOG(LogTemp, Warning,
-    //            TEXT("LAN = %d"),
-    //            Session->SessionSettings.bIsLANMatch);
-    //    }
-    //    else
-    //    {
-    //        UE_LOG(LogTemp, Warning,
-    //            TEXT("After Travel Session Missing"));
-    //    }
-    //}
-
-
-
-
-   //// 클라이언트는 BeginPlay 시점에 GameState가 아직 없을 수도 있음
-   //if (AShelterGameState* GS = GetWorld()->GetGameState<AShelterGameState>())
-   //{
-   //   GS->OnPlayerListChanged.AddDynamic(
-   //       this,
-   //       &AShelterPlayerController::HandlePlayerListChanged);
-   //   
-   //   // 최초 1회 갱신
-   //   HandlePlayerListChanged();
-   //}
-
 }
 
-void AShelterPlayerController::HandlePlayerListChanged()
+void AShelterPlayerController::ClientReceiveChatMessage_Implementation(const FString& PlayerName, const FString& Message)
 {
-    BP_OnPlayerListChanged();
+	OnChatMessageReceived.Broadcast(PlayerName, Message);
 }
+
+void AShelterPlayerController::ServerSendChatMessage_Implementation(const FString& Message)
+{
+	if (Message.TrimStartAndEnd().IsEmpty())
+	{
+		return;
+	}
+
+	int32 PlayerIndex = 0;
+
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
+	{
+		AShelterPlayerController* PC =
+			Cast<AShelterPlayerController>(It->Get());
+
+		if (PC)
+		{
+			if (PC == this)
+			{
+				break;
+			}
+
+			++PlayerIndex;
+		}
+	}
+
+	FString PlayerName = FString::Printf(
+		TEXT("Player_%d"),
+		PlayerIndex
+	);
+
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
+	{
+		AShelterPlayerController* PC =
+			Cast<AShelterPlayerController>(It->Get());
+
+		if (PC)
+		{
+			PC->ClientReceiveChatMessage(PlayerName, Message);
+		}
+	}
+
+}
+
+
+
+
+
 
 
--- Source/HeavyHanded/Public/Core/PlayerControllers/ShelterPlayerController.h
+++ Source/HeavyHanded/Public/Core/PlayerControllers/ShelterPlayerController.h
@@ -9,7 +9,13 @@
 /**
  * 
  */
-class AMyGameState;
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+	FOnChatMessageReceived,
+	const FString&, PlayerName,
+	const FString&, Message
+);
 
 UCLASS()
 class HEAVYHANDED_API AShelterPlayerController : public APlayerController
@@ -17,17 +23,28 @@ class HEAVYHANDED_API AShelterPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 
-public:
-
-    UFUNCTION(BlueprintImplementableEvent)
-    void BP_OnPlayerListChanged();
 
 protected:
 
     virtual void BeginPlay() override;
 
-    UFUNCTION()
-    void HandlePlayerListChanged();
+
+public:
+
+	// 클라이언트 → 서버
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void ServerSendChatMessage(const FString& Message);
+
+	// 서버 → 해당 클라이언트
+	UFUNCTION(Client, Reliable)
+	void ClientReceiveChatMessage(
+		const FString& PlayerName,
+		const FString& Message
+	);
+
+	// 채팅 UI에서 바인딩
+	UPROPERTY(BlueprintAssignable)
+	FOnChatMessageReceived OnChatMessageReceived;
 
 
 };
